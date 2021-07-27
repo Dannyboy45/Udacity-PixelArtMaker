0d57c200927e10301e6eac60c2957a65e6a48d9c
Daniel <daniel.okonov@gmail.com>
2021-07-26 18:31:36 -0700

    canvas cleaned up before submit

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,15 +1,26 @@
-function makeGrid() {       
+
+window.addEventListener('load', function() {
+    const form = document.getElementById('sizePicker');
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        makeGrid();
+    });
+})
+
+function makeGrid() {    
+    
+    const tableGrid = document.getElementById('pixelCanvas');
+    tableGrid.innerHTML = '';
 
     const numOfRows = parseInt(document.getElementById('inputHeight').value);
-    const numOfColumns = parseInt(document.getElementById('inputWidth').value);
-
-    const tableGrid = document.getElementById('pixelCanvas');
+    const numOfColumns = parseInt(document.getElementById('inputWidth').value);    
 
     for(let i = 0; i < numOfRows; i++){
         const row = document.createElement('tr');
 
         for(let j = 0; j < numOfColumns; j++){
-            const cell = document.createElement('td', 'blah');
+            const cell = document.createElement('td');
 
             cell.addEventListener('click', event => {
                 event.target.style.backgroundColor = "blue";
